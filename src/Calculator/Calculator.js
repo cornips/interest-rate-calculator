cbd5751d2e32eb7d8a02d9cfa09fe7aeef09d53c
Martijn Cornips <martijn@cornips.nl>
2020-03-21 15:37:49 +0100

    Add max loan calculator

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -6,6 +6,14 @@ class Calculator extends Component {
   constructor(props) {
     super(props);
 
+    // Set initial values
+    this.state = {
+      product: "marketing",
+      legal_form: "bv"
+    };
+    this.saveMaxLoanToState();
+
+    // prepare select fields
     this.selects = [
       {
         name: "product",
@@ -14,28 +22,54 @@ class Calculator extends Component {
           marketing: "Marketing",
           equipment: "Equipment"
         },
-        callback: this.saveToState
+        callback: this.saveInputToState
       },
       {
-        name: "legal-form",
+        name: "legal_form",
         label: "Legal form",
         options: {
           bv: "BV",
           eenmanszaak: "Eenmanszaak"
         },
-        callback: this.saveToState
+        callback: this.saveInputToState
       }
     ];
   }
 
-  saveToState = event => {
+  // Callback function to allow input to be saved in state
+  saveInputToState = event => {
     event.persist();
 
     const { name, value } = event.target;
 
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, this.saveMaxLoanToState);
 
     return null;
+  };
+
+  // Function to allow max loan to be saved in state
+  saveMaxLoanToState = () => {
+    const { product = null, legal_form = null } = this.state;
+    if (!product || !legal_form) return null;
+
+    const maxLoan = this.calculateMaxLoan(product, legal_form);
+    console.log(maxLoan);
+    this.setState({ maxLoan: maxLoan });
+
+    return null;
+  };
+
+  // Function to calculate max loan based on input
+  calculateMaxLoan = (product, legalForm) => {
+    let loan = { amount: null, duration: null };
+    if (product === "marketing") {
+      loan.amount = 250000;
+      loan.duration = 36;
+    } else if (product === "equipment") {
+      loan.amount = legalForm === "bv" ? 500000 : 250000;
+      loan.duration = 60;
+    }
+    return loan;
   };
 
   render() {
